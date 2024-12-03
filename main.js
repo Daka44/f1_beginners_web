59dbe6cf1850a8ac7c2bfeb7de5c60e563d1f7f0
Daka44 <super080404@gmail.com>
2024-12-03 13:06:11 +0900

    start.html추가

---
+++ main.js
@@ -0,0 +1,13 @@
+// // main.js
+// function showStartContent() {
+//     console.log("버튼 클릭됨!"); // 버튼 클릭 시 콘솔에 메시지 출력
+//
+//     const startContent = document.getElementById('start-content');
+//     startContent.classList.remove('hidden'); // 숨김 상태 제거
+//
+//     // 부드럽게 스크롤 이동
+//     window.scrollTo({
+//         top: startContent.offsetTop,
+//         behavior: 'smooth'
+//     });
+// }
